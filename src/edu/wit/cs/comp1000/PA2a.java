0cc3269a973628c24f3e356c823aec2541d8b910
Alexander Santangelo <santangeloa@wit.edu>
2024-10-10 00:31:32 -0400

    PA2 Submission

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,81 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+/**
+ * Solution to the PA2a assignment.
+ * When it runs, it ask the user to input five whole numbers, which finds the sum of the positive, non-positive, and every number input by the user.
+ * 
+ * @author Alexander Santangelo
+ */
+import java.util.Scanner;
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * Starts the program, user inputs 5 whole number when prompted, finds the sum of the positive, non-positive, and every number input by user.
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		
+		int sumPositive = 0, sumNonPositive = 0, sumAll = 0;
+		int countPositive = 0, countNonPositive = 0;
+		
+		System.out.printf("Enter five whole numbers: "); 
+		for (int i = 0; i < 5; i++) {
+			int num = input.nextInt();
+			sumAll += num;
+			
+			if(num > 0) {
+				sumPositive += num;
+				countPositive++;
+			} else {
+				sumNonPositive += num;
+				countNonPositive++;
+			}
+		}
+		
+		double avgPositive;
+		if(countPositive >0 ) {
+			avgPositive = (double) sumPositive / countPositive;
+		} else {
+			avgPositive = 0.0;
+		}
+		
+		double avgNonPositive;
+		if(countNonPositive >0) {
+			avgNonPositive = (double) sumNonPositive / countNonPositive;
+		} else {
+			avgNonPositive = 0.0;
+		}
+		
+		double avgAll = (double) sumAll/5;
+		
+		if(countPositive == 1) {
+			System.out.printf("The sum of the " + countPositive + " positive number: " + sumPositive + "%n");
+		}else {
+			System.out.printf("The sum of the " + countPositive + " positive numbers: " + sumPositive + "%n");
+		}
+		
+		if(countNonPositive == 1) {
+			System.out.printf("The sum of the " + countNonPositive + " non-positive number: " + sumNonPositive + "%n");
+		}else {
+			System.out.printf("The sum of the " + countNonPositive + " non-positive numbers: " + sumNonPositive + "%n");
+		}
+		
+		System.out.printf("The sum of the 5 numbers: " + sumAll + "%n");
+		
+		if(countPositive == 1) {
+			System.out.printf("The average of the " + countPositive + " positive number: %.2f%n", avgPositive);
+		}else {
+			System.out.printf("The average of the " + countPositive + " positive numbers: %.2f%n", avgPositive);
+		}
+		if(countNonPositive == 1) {
+			System.out.printf("The average of the " + countNonPositive + " non-positive number: %.2f%n", avgNonPositive);
+		}else {
+			System.out.printf("The average of the " + countNonPositive + " non-positive numbers: %.2f%n", avgNonPositive);
+		}
+		
+		System.out.printf("The average of the 5 numbers: %.2f%n", avgAll);
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,53 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+/**
+ * Solution to the PA2b assignment.
+ * When it runs, it asks the user for values for a, b, and c, and outputs the roots after using the quadratic formula.
+ * 
+ * @author Alexander Santangelo
+ */
+import java.util.Scanner;
 public class PA2b {
 
-	// TODO: document this function
+	/**
+	 * Starts the program, user inputs a value for a, b, and c, outputs the roots after using the quadratic formula.
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Enter a b c: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		
+		if (a == 0) {
+			System.out.println("The value of 'a' cannot be zero");
+			return;
+		}
+		double root1 = 0, root2 = 0;
+		
+		if (b*b - 4*a*c<0) {
+			System.out.println("Roots: imaginary");
+		}else if (b*b-4*a*c == 0) {
+			root1 = -b/(2*a);
+			System.out.printf("Root: %.2f%n", root1);
+			return;
+		}else
+			root1 = (-b - Math.sqrt(b*b-4*a*c)) / (2*a);
+			root2 = (-b + Math.sqrt(b*b-4*a*c)) / (2*a);
+			
+			if(root1 > root2) {
+				double temp = root1;
+					root1 = root2;
+					root2 = temp;
+			}
+			if (b*b - 4*a*c<0) {
+			
+			}else {
+				System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+			}
+		}
 	}
 
-}
